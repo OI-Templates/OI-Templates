982e88fac20bb83276bcbd5d812389f89cbfce81
Muzi-flute <teddy_di@foxmail.com>
2018-07-28 17:30:00 +0800

    upd

--- oi-template/main.cpp
+++ oi-template/main.cpp
@@ -1,1 +1,11 @@
 #include<cstdio>
+#include<algorithm>
+#include<queue>
+using namespace std;
+const int maxn = 1e5 + 5;
+int fa[maxn];
+template<class T>
+inline T find_root(T a)
+{
+	return a == fa[a] ? a : fa[a] = find_root(fa[a]);
+}
